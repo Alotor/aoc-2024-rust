99dc947ab09aeb32ff969b931367cefe048dc17c
alonso.torres <alonso.torres@kaleidos.net>
2024-12-17 17:28:25 +0100

    Day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -2,6 +2,7 @@
 
 use regex::Regex;
 use memoize::memoize;
+use nalgebra::*;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct Entry {
@@ -44,7 +45,9 @@ fn parse(input: &str) -> Input {
         .collect()
 }
 
-fn min_tokens(entry: Entry) -> Option<u32> {
+// My naive solution using recursion. Left here for historic purposes :P
+/*
+fn min_tokens_long(entry: Entry, _: f64) -> Option<u32> {
     #[memoize]
     fn min_tokens_rec(entry: Entry, tok_a: u32, tok_b: u32, px: u32, py: u32) -> Option<u32> {
         if px == entry.price_x && py == entry.price_y {
@@ -70,18 +73,58 @@ fn min_tokens(entry: Entry) -> Option<u32> {
     }
     min_tokens_rec(entry, 0, 0, 0, 0)
 }
+*/
+
+fn try_u64(num: f64) -> Option<u64> {
+    if num.fract() <= 0.0001  {
+        Some(num as u64)
+    } else if num.fract() >= 0.9999 {
+        Some(num as u64 + 1)
+    } else {
+        None
+    }
+}
+
+fn min_tokens(entry: Entry, modifier: f64) -> Option<u64> {
+    let m = Matrix2::<f64>::new(
+        entry.ax.into(),
+        entry.ay.into(),
+        entry.bx.into(),
+        entry.by.into());
+
+    let r = Matrix1x2::<f64>::new(
+        modifier + entry.price_x as f64,
+        modifier + entry.price_y as f64);
+
+    if let Some(inverse) = m.try_inverse() {
+        let sol = r * inverse;
+        let sa = sol[0];
+        let sb = sol[1];
+
+        if let (Some(a), Some(b)) = (try_u64(sa), try_u64(sb)) {
+            Some(3 * a + b)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
 
 #[aoc(day13, part1)]
-fn part1(input: &Input) -> u32 {
+fn part1(input: &Input) -> u64 {
     input
         .iter()
-        .map(|entry| min_tokens(*entry).unwrap_or(0))
+        .map(|entry| min_tokens(*entry, 0.0).unwrap_or(0))
         .sum()
 }
 
 #[aoc(day13, part2)]
-fn part2(input: &Input) -> u32 {
-    todo!()
+fn part2(input: &Input) -> u64 {
+    input
+        .iter()
+        .map(|entry| min_tokens(*entry, 1e13).unwrap_or(0))
+        .sum()
 }
 
 
@@ -118,22 +161,26 @@ mod tests {
 
     #[test]
     fn test_min_tokens() {
-        
-        assert_eq!(min_tokens(
-            Entry { ax: 94, ay: 34, bx: 22, by: 67, price_x: 8400, price_y: 5400 }),
-            Some(280));
-        
-        assert_eq!(min_tokens(
-            Entry { ax: 26, ay: 66, bx: 67, by: 21, price_x: 12748, price_y: 12176 }),
-            None);
-        
-        assert_eq!(min_tokens(
-            Entry { ax: 17, ay: 86, bx: 84, by: 37, price_x: 7870, price_y: 6450 }),
-            Some(200));
-        
-        assert_eq!(min_tokens(
-            Entry { ax: 69, ay: 23, bx: 27, by: 71, price_x: 18641, price_y: 10279 }),
-            None);
+        assert_eq!(min_tokens(Entry { ax: 94, ay: 34, bx: 22, by: 67, price_x: 8400, price_y: 5400 }, 0.0), Some(280));
+        assert_eq!(min_tokens(Entry { ax: 26, ay: 66, bx: 67, by: 21, price_x: 12748, price_y: 12176 }, 0.0), None);
+        assert_eq!(min_tokens(Entry { ax: 17, ay: 86, bx: 84, by: 37, price_x: 7870, price_y: 6450 }, 0.0), Some(200));
+        assert_eq!(min_tokens(Entry { ax: 69, ay: 23, bx: 27, by: 71, price_x: 18641, price_y: 10279 }, 0.0), None);
+
+        // I got these cases by executing my naive solution
+        assert_eq!(min_tokens(Entry { ax: 98, ay: 41, bx: 59, by: 88, price_x: 6029, price_y: 6258 }, 0.0), Some(137));
+        assert_eq!(min_tokens(Entry { ax: 31, ay: 88, bx: 75, by: 36, price_x: 8434, price_y: 8728 }, 0.0), Some(278));
+        assert_eq!(min_tokens(Entry { ax: 17, ay: 90, bx: 34, by: 11, price_x: 1496, price_y: 5047 }, 0.0), Some(179));
+        assert_eq!(min_tokens(Entry { ax: 52, ay: 99, bx: 47, by: 15, price_x: 8241, price_y: 8316 }, 0.0), Some(306));
+        assert_eq!(min_tokens(Entry { ax: 34, ay: 35, bx: 12, by: 96, price_x: 3226, price_y: 7085 }, 0.0), Some(282));
+        assert_eq!(min_tokens(Entry { ax: 98, ay: 44, bx: 51, by: 74, price_x: 7348, price_y: 5752 }, 0.0), Some(198));
+        assert_eq!(min_tokens(Entry { ax: 27, ay: 17, bx: 20, by: 89, price_x: 3108, price_y: 7229 }, 0.0), Some(261));
+        assert_eq!(min_tokens(Entry { ax: 98, ay: 56, bx: 37, by: 66, price_x: 4967, price_y: 3870 }, 0.0), Some(149));
+        assert_eq!(min_tokens(Entry { ax: 36, ay: 55, bx: 63, by: 12, price_x: 2988, price_y: 4228 }, 0.0), Some(232));
+        assert_eq!(min_tokens(Entry { ax: 87, ay: 17, bx: 28, by: 54, price_x: 8764, price_y: 4236 }, 0.0), Some(304));
+        assert_eq!(min_tokens(Entry { ax: 43, ay: 45, bx: 69, by: 17, price_x: 4224, price_y: 1660 }, 0.0), Some(104));
+        assert_eq!(min_tokens(Entry { ax: 27, ay: 66, bx: 53, by: 29, price_x: 4020, price_y: 4095 }, 0.0), Some(168));
+        assert_eq!(min_tokens(Entry { ax: 80, ay: 86, bx: 18, by: 94, price_x: 4434, price_y: 7230 }, 0.0), Some(177));
+        assert_eq!(min_tokens(Entry { ax: 78, ay: 67, bx: 16, by: 65, price_x: 6426, price_y: 7365 }, 0.0), Some(261));
     }
 
     #[test]
@@ -154,7 +201,29 @@ mod tests {
              Button A: X+69, Y+23
              Button B: X+27, Y+71
              Prize: X=18641, Y=10279");
-
+        
         assert_eq!(part1(&input), 480);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = parse(
+            "Button A: X+94, Y+34
+             Button B: X+22, Y+67
+             Prize: X=8400, Y=5400
+             
+             Button A: X+26, Y+66
+             Button B: X+67, Y+21
+             Prize: X=12748, Y=12176
+             
+             Button A: X+17, Y+86
+             Button B: X+84, Y+37
+             Prize: X=7870, Y=6450
+             
+             Button A: X+69, Y+23
+             Button B: X+27, Y+71
+             Prize: X=18641, Y=10279");
+        
+        assert_eq!(part2(&input), 875318608908);
     }
 }
