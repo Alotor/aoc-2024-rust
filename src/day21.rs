89f517de6491e4875404d2042fd43bbd94d432b4
alonso.torres <alonso.torres@kaleidos.net>
2024-12-22 00:50:03 +0100

    Day 21 Part 2

--- src/day21.rs
+++ src/day21.rs
@@ -193,59 +193,9 @@ fn move_to(numpad: NumpadType, from_key: char, to_key: char) -> Vec<Vec<char>> {
     result
 }
 
-fn join(a: Vec<Vec<char>>, b: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    if a.is_empty() {
-        return b.clone();
-    }
-
-    if b.is_empty() {
-        return a.clone();
-    }
-
-    let mut result = Vec::<Vec<char>>::new();
-    for i in 0 .. a.len() {
-        for j in 0 .. b.len() {
-            let mut s = a[i].clone();
-            s.append(&mut b[j].clone());
-            result.push(s);
-        }
-    }
-    result
-}
-
-fn generate_keys(numpad: NumpadType, keys: Vec<char>, level: u32) -> Vec<Vec<char>> {
-    let mut result = Vec::<Vec<char>>::from([]);
-    let mut prev_key = 'A';
-    
-    for k in keys {
-        let mut keys = move_to(numpad, prev_key, k);
-        result = join(result, keys);
-        prev_key = k;
-    }
-
-    if level > 1 {
-        let mut rr = Vec::<Vec<char>>::new();
-        
-        for r in result {
-            let mut rl = generate_keys(TypeB, r, level - 1);
-            rr.append(&mut rl);
-        }
-        return rr;
-    }
-
-    // println!("{}", String::from_iter(&result));
-    result
-}
-
-fn seq_length(sequence: Vec<char>) -> u64 {
-    let r = generate_keys(TypeA, sequence, 3);
-    let m = r.iter().min_by(|l1, l2| l1.len().cmp(&l2.len())).unwrap();
-    m.len() as u64
-}
-
-fn score_complexity(sequence: Vec<char>) -> u64 {
-    let num: u64 = String::from_iter(&sequence[.. sequence.len() - 1]).parse().unwrap();
-    seq_length(sequence) * num
+fn score_complexity(keys: Vec<char>, levels: u32) -> u64 {
+    let num: u64 = String::from_iter(&keys[.. keys.len() - 1]).parse().unwrap();
+    min_seq_length(TypeA, keys, levels + 1) * num
 }
 
 fn evaluate(numpad: NumpadType, seq: Vec<char>) -> Vec<char> {
@@ -265,6 +215,32 @@ fn evaluate(numpad: NumpadType, seq: Vec<char>) -> Vec<char> {
     result
 }
 
+#[memoize]
+fn min_seq_length(numpad: NumpadType, keys: Vec<char>, level: u32) -> u64 {
+    if level == 0 {
+        return 1;
+    }
+    
+    let mut prev_key = 'A';
+    let mut result = 0;
+
+    for key in &keys {
+        let keys = move_to(numpad, prev_key, *key);
+
+        let min_seq = keys
+            .iter()
+            .map(|keys| min_seq_length(TypeB, keys.clone(), level - 1))
+            .min()
+            .unwrap();
+
+        result += min_seq;
+        prev_key = *key;
+    }
+
+    result
+}
+
+
 #[aoc_generator(day21)]
 fn parse(input: &str) -> Input {
     input
@@ -276,16 +252,19 @@ fn parse(input: &str) -> Input {
 #[aoc(day21, part1)]
 fn part1(input: &Input) -> u64 {
     let mut out: u64 = 0;
-    
     for i in input {
-        out += score_complexity(i.clone())
+        out += score_complexity(i.clone(), 3);
     }
     out
 }
 
 #[aoc(day21, part2)]
 fn part2(input: &Input) -> u64 {
-    todo!()
+    let mut out: u64 = 0;
+    for i in input {
+        out += score_complexity(i.clone(), 26);
+    }
+    out
 }
 
 #[cfg(test)]
@@ -336,124 +315,14 @@ mod tests {
     }
 
     #[test]
-    fn test_join_lists() {
-
-        let a = vec![vec![]];
-        let b = vec![vec!['a','a']];
-        assert_eq!(join(a, b), vec![vec!['a','a']]);
-
-        let a = vec![vec!['a','a']];
-        let b = vec![vec!['b','b']];
-        assert_eq!(join(a, b), vec![
-            vec!['a','a','b','b']
-        ]);
-        
-        let a = vec![vec!['a','a']];
-        let b = vec![vec!['b','b'], vec!['c','c']];
-        assert_eq!(join(a, b), vec![
-            vec!['a','a','b','b'],
-            vec!['a','a','c','c'],
-        ]);
-
-        let a = vec![vec!['a','a'], vec!['b','b']];
-        let b = vec![vec!['c','c'], vec!['d','d']];
-        assert_eq!(join(a, b), vec![
-            vec!['a','a','c','c'],
-            vec!['a','a','d','d'],
-            vec!['b','b','c','c'],
-            vec!['b','b','d','d'],
-        ]);
+    fn test_min_seq_length() {
+        assert_eq!(min_seq_length(TypeA, "029A".chars().collect(), 4), 68);
+        assert_eq!(min_seq_length(TypeA, "980A".chars().collect(), 4), 60);
+        assert_eq!(min_seq_length(TypeA, "179A".chars().collect(), 4), 68);
+        assert_eq!(min_seq_length(TypeA, "456A".chars().collect(), 4), 64);
+        assert_eq!(min_seq_length(TypeA, "379A".chars().collect(), 4), 64);
     }
-
-    #[test]
-    fn test_generate_keys() {
-        assert_eq!(
-            generate_keys(TypeA, "029A".chars().collect(), 1),
-            vec![vec!['<', 'A', '^', 'A', '^', '^', '>', 'A', 'v', 'v', 'v', 'A'],
-                 vec!['<', 'A', '^', 'A', '^', '>', '^', 'A', 'v', 'v', 'v', 'A'],
-                 vec!['<', 'A', '^', 'A', '>', '^', '^', 'A', 'v', 'v', 'v', 'A']]);
-
-        let r = generate_keys(TypeA, "029A".chars().collect(), 3);
-        let m = r.iter().min_by(|l1, l2| l1.len().cmp(&l2.len())).unwrap();
-        assert_eq!(m.len(), 68);
-
-        let r = generate_keys(TypeA, "980A".chars().collect(), 3);
-        let m = r.iter().min_by(|l1, l2| l1.len().cmp(&l2.len())).unwrap();
-        assert_eq!(m.len(), 60);
-
-        let r = generate_keys(TypeA, "179A".chars().collect(), 3);
-        let m = r.iter().min_by(|l1, l2| l1.len().cmp(&l2.len())).unwrap();
-        assert_eq!(m.len(), 68);
-
-        let r = generate_keys(TypeA, "456A".chars().collect(), 3);
-        let m = r.iter().min_by(|l1, l2| l1.len().cmp(&l2.len())).unwrap();
-        assert_eq!(m.len(), 64);
-
-        let r = generate_keys(TypeA, "379A".chars().collect(), 3);
-        let m = r.iter().min_by(|l1, l2| l1.len().cmp(&l2.len())).unwrap();
-        assert_eq!(m.len(), 64);
-    }
-
-    
-    /*
-    #[test]
-    fn test_sequence() {
-        let input = vec!['3','7','9','A'];
-        assert_eq!(
-            evaluate(TypeA, generate_keys(TypeA, 'A', input.clone())),
-            input
-        );
-
-        let input = generate_keys(TypeA, 'A', vec!['3','7','9','A']);
-        assert_eq!(
-            evaluate(TypeB, generate_keys(TypeB, 'A', input.clone())),
-            input
-        );
-
-        let input = generate_keys(TypeB, 'A', generate_keys(TypeB, 'A', generate_keys(TypeA, 'A', vec!['3','7','9','A'])));
-        assert_eq!(
-            evaluate(TypeB, generate_keys(TypeB, 'A', input.clone())),
-            input
-        );
-
-        //assert_eq!(
-        //    generate_keys(TypeB, 'A', vec!['<','A','^','A','^','^','>','A','v','v','v','A'],).len(),
-        //    28
-        //);
-        //assert_eq!(
-        //    generate_keys(TypeB, 'A', vec!['v','<','<','A','>','>','^','A','<','A','>','A','v','A','<','^','A','A','>','A','<','v','A','A','A','>','^','A'],).len(),
-        //    68
-        //);
-
-        println!("=====");
-
-        // println!("{}", String::from_iter(evaluate(TypeB, "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A".chars().collect())));
-        // println!("{}", String::from_iter(evaluate(TypeB, "<A>Av<<AA>^AA>AvAA^A<vAAA>^A".chars().collect())));
-        // println!("{}", String::from_iter(evaluate(TypeA, "^A<<^^A>>AvvvA".chars().collect())));
-
-        println!("{}", String::from_iter(generate_keys(TypeA, 'A', "379A".chars().collect())));
-        println!("{}", String::from_iter(generate_keys(TypeB, 'A', "^A<<^^A>>AvvvA".chars().collect())));
-        println!("{}", String::from_iter(generate_keys(TypeB, 'A', "<A>Av<<AA>^AA>AvAA^A<vAAA>^A".chars().collect())));
-
-        println!("=====");
-
-        // assert_eq!(
-        //     generate_keys(TypeB, 'A', generate_keys(TypeB, 'A', generate_keys(TypeA, 'A', vec!['3','7','9','A'],))).len(),
-        //     64);
-
-
-    }
-
-    */
-
-    #[test]
-    fn test_score() {
-        assert_eq!(seq_length(vec!['0','2','9','A']), 68);
-        assert_eq!(seq_length(vec!['9','8','0','A']), 60);
-        assert_eq!(seq_length(vec!['1','7','9','A']), 68);
-        assert_eq!(seq_length(vec!['4','5','6','A']), 64);
-        assert_eq!(seq_length(vec!['3','7','9','A']), 64);
-    }
+   
 }
 
 
