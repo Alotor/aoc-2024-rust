a9e2a7c2b00120515c13e66605594259ad676b1f
alonso.torres <alonso.torres@kaleidos.net>
2024-12-26 10:19:38 +0100

    Day 24 - Part 2

--- src/day24.rs
+++ src/day24.rs
@@ -2,8 +2,9 @@
 
 use std::collections::{HashSet, HashMap};
 
+const BITS: usize = 46;
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 enum Operand {
     OR,
     AND,
@@ -12,7 +13,7 @@ enum Operand {
 
 use Operand::*;
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 struct Operation {
     op: Operand,
     in_a: String,
@@ -116,17 +117,33 @@ fn parse(input: &str) -> Input {
     (signals, operands)
 }
 
-fn eval_circuit(signals: &mut Signals, operations: &Operations) {
+fn swap(swaps: &HashMap<String, String>, operation: Operation) -> Operation {
+    let mut result = operation.clone();
+    
+    if let Some(x) = swaps.get(&result.result) {
+        result.result = x.clone();
+    }
+    // if let Some(x) = swaps.get(&result.in_a) {
+    //     result.in_a = x.clone();
+    // }
+    // if let Some(x) = swaps.get(&result.in_b) {
+    //     result.in_b = x.clone();
+    // }
+    result
+}
+
+fn eval_circuit(signals: &mut Signals, operations: &Operations, swaps: &HashMap<String, String>) {
     let s2 = signals.clone();
-    let mut to_eval = Vec::from_iter(s2.keys());
+    let mut to_eval = Vec::<String>::from_iter(s2.keys().map(|s| s.clone()));
     let mut evaluated = HashSet::<String>::from_iter(signals.keys().map(|s| s.clone()));
 
     while let Some(s) = to_eval.pop() {
-        if let Some(ops) = operations.get(s) {
+        if let Some(ops) = operations.get(&s) {
             ops.iter().for_each(|op| {
+                let op = swap(swaps, op.clone());
                 if !evaluated.contains(&op.result) && op.eval(signals) {
                     evaluated.insert(op.result.clone());
-                    to_eval.push(&op.result);
+                    to_eval.push(op.result);
                 }
             });
         }
@@ -136,7 +153,7 @@ fn eval_circuit(signals: &mut Signals, operations: &Operations) {
 #[aoc(day24, part1)]
 fn part1((signals, operations): &Input) -> u64 {
     let mut signals = signals.clone();
-    eval_circuit(&mut signals, &operations);
+    eval_circuit(&mut signals, &operations, &HashMap::new());
 
 
     let mut zsig: Vec<_> = signals
@@ -157,38 +174,229 @@ fn part1((signals, operations): &Input) -> u64 {
     result
 }
 
-fn reverse_operations(operations: &Operations) -> OperationsResult {
+fn reverse_operations(operations: &Operations, swaps: &HashMap<String, String>) -> OperationsResult {
     let mut result = OperationsResult::new();
 
     for (k, v) in operations {
         for op in v {
-            result.insert(op.result.clone(), op.clone());
+            let op = swap(swaps, op.clone());
+            result.insert(op.result.clone(), op);
         }
     }
 
     result
 }
 
-fn dependencies(signal: String, signals: &Signals, rev_opers: &OperationsResult) -> HashSet<String> {
+fn dependencies(signal: String, rev_opers: &OperationsResult) -> HashSet<String> {
     let mut result = HashSet::<String>::new();
     let mut pending = Vec::<&String>::new();
     pending.push(&signal);
 
     while let Some(s) = pending.pop() {
         if let Some(op) = rev_opers.get(s) {
-            println!("+ {s} => {}, {}", &op.in_a, &op.in_b);
+            // println!("+ {s} <- {} {:?} {}", &op.in_a, &op.op, &op.in_b);
             result.insert(op.in_a.clone());
             result.insert(op.in_b.clone());
-            pending.push(&op.in_a);
             pending.push(&op.in_b);
+            pending.push(&op.in_a);
         }
     }
     result
 }
 
+fn dependencies_with_value(signal: String, signals: &Signals, rev_opers: &OperationsResult) -> HashSet<String> {
+    let mut result = HashSet::<String>::new();
+    let mut pending = Vec::<&String>::new();
+    pending.push(&signal);
+
+    while let Some(s) = pending.pop() {
+        if let Some(op) = rev_opers.get(s) {
+            println!("+ {s}({}) <- {}({}) {:?} {}({})",
+                     signals.get(&op.result).unwrap_or(&0),
+                     &op.in_a, signals.get(&op.in_a).unwrap_or(&0),
+                     &op.op,
+                     &op.in_b, signals.get(&op.in_b).unwrap_or(&0));
+            result.insert(op.in_a.clone());
+            result.insert(op.in_b.clone());
+            pending.push(&op.in_b);
+            pending.push(&op.in_a);
+        }
+    }
+    result
+}
+
+fn to_b(num: u64, digits: usize) -> Vec<u8> {
+    let mut result = Vec::new();
+    // let digits = num.ilog2() + 1;
+
+    let mut cur = num;
+    for d in 0 .. digits {
+        result.push((cur % 2u64) as u8);
+        cur = cur / 2;
+    }
+    result.reverse();
+    result
+}
+
+fn from_b(num: Vec<u8>) -> u64 {
+    let mut result = 0;
+    let digits = num.len();
+
+    for i in 0 .. digits {
+        let exp = (digits - i - 1) as u32;
+        result += num[i] as u64 * 2u64.pow(exp);
+    }
+    result
+}
+
+
+// The problem is 45bit input and result 46bit
+fn execute(operations: &Operations, a: u64, b: u64, swaps: &HashMap<String, String>) -> (u64, Signals) {
+    let x = to_b(a, BITS);
+    let y = to_b(b, BITS);
+
+    // println!("X={x:?}, {}", from_b(x.clone()));
+    // println!("Y={y:?}, {}", from_b(y.clone()));
+
+    let mut signals = Signals::new();
+
+    for i in 0 .. x.len() {
+        let j = x.len() - i - 1;
+        signals.insert(format!("x{:02}", i), x[j]);
+    }
+
+    for i in 0 .. y.len() {
+        let j = y.len() - i - 1;
+        signals.insert(format!("y{:02}", i), y[j]);
+    }
+
+    eval_circuit(&mut signals, operations, swaps);
+
+    let mut z = Vec::<u8>::with_capacity(46);
+
+    for i in 0 .. BITS {
+        let i = BITS - i - 1;
+        let key = format!("z{:02}", i);
+        let v = *signals.get(&key).unwrap_or(&0);
+        z.push(v);
+    }
+
+    // println!("Z={z:?}, {}", from_b(z.clone()));
+
+    (from_b(z), signals)
+}
+
+
+// #[aoc(day24, part2)]
+fn show_bad_gates((signals, operations): &Input) -> u32 {
+    let kk: HashSet<_> = operations
+        .iter()
+        .flat_map(|(_, ops)| ops)
+        .filter(|op| {
+            if op.result.starts_with("z") && op.op != XOR {
+                return true;
+            }
+            if !op.result.starts_with("z") &&
+                !op.in_a.starts_with("x") &&
+                !op.in_b.starts_with("x") &&
+                !op.in_a.starts_with("y") &&
+                !op.in_b.starts_with("y") &&
+                op.op == XOR
+            {
+                return true;
+            }
+                
+            false
+        })
+        .collect();
+
+    for k in kk {
+        println!("{:?}", k);
+    }
+    
+    0
+}
+
+// I've solved this by using this method to display bad results and then
+// by hand checking the bas gates and replacing them.
+// The swaps map is a relationship of the gates to be replaced.
 #[aoc(day24, part2)]
-fn part2(input: &Input) -> u32 {
-    todo!()
+fn check_sums((signals, operations): &Input) -> String {
+    let mut swaps = HashMap::<String, String>::new();
+    swaps.insert(String::from("z08"), String::from("cdj"));
+    swaps.insert(String::from("cdj"), String::from("z08"));
+    
+    swaps.insert(String::from("z16"), String::from("mrb"));
+    swaps.insert(String::from("mrb"), String::from("z16"));
+    
+    swaps.insert(String::from("z32"), String::from("gfm"));
+    swaps.insert(String::from("gfm"), String::from("z32"));
+    
+    swaps.insert(String::from("qjd"), String::from("dhm"));
+    swaps.insert(String::from("dhm"), String::from("qjd"));
+    
+    let op_results = reverse_operations(&operations, &swaps);
+
+    // let r = 0 ..= 2u64.pow(16);
+    // let r = [256];
+
+    let mut x = 1;
+    
+    loop {
+        // let x = x / 100;
+        // let y = x % 100;
+        // let x = 0;
+        x *= 2;
+        let y = x;
+
+        if x >= 2u64.pow(BITS as u32) {
+            break;
+        }
+        
+        // println!("=== {x} + {y} = {} ===", x + y);
+        
+        let (result, signals) = execute(&operations, x, y, &swaps);
+
+        let real = to_b(result, BITS);
+        let expected = to_b(x + y, BITS);
+
+        for i in 0 .. BITS {
+            if real[i] != expected[i] {
+                let j = BITS - i - 1;
+                println!("DIFF z{j:02} expected: {} but is {}", expected[i], real[i]);
+                let deps = dependencies_with_value(format!("z{j:02}"), &signals, &op_results);
+                println!("");
+            }
+        }
+
+        
+        if real != expected {
+            // println!(">>>>>> {}", &signals.get(&String::from("z32")).unwrap_or(&0));
+            // dependencies_with_value(format!("z32"), &signals, &op_results);
+            // 
+            // println!(">>>>>> {}", &signals.get(&String::from("z31")).unwrap_or(&0));
+            // dependencies_with_value(format!("z31"), &signals, &op_results);
+            // 
+            // println!(">>>>>> {}", &signals.get(&String::from("pqv")).unwrap_or(&0));
+            // dependencies_with_value(format!("pqv"), &signals, &op_results);
+            // 
+            // println!(">>>>>> {}", &signals.get(&String::from("gfm")).unwrap_or(&0));
+            // dependencies_with_value(format!("gfm"), &signals, &op_results);
+
+            // println!("{result}, {} ==> {:?}", x + 1, to_b(result ^ (x+y), BITS));
+            panic!();
+        }
+
+    }
+
+    // Output format: gates on alphabetical order
+    let mut gates: Vec<_> = swaps
+        .keys()
+        .map(|k| k.clone())
+        .collect();
+
+    gates.sort();
+    gates.join(",")
 }
 
 
@@ -317,7 +525,7 @@ mod tests {
     #[test]
     fn test_eval_circuit() {
         let (mut signals, operations) = sample_input_1();
-        eval_circuit(&mut signals, &operations);
+        eval_circuit(&mut signals, &operations, &HashMap::new());
 
         assert_eq!(*signals.get(&String::from("z00")).unwrap(), 0);
         assert_eq!(*signals.get(&String::from("z01")).unwrap(), 0);
@@ -336,7 +544,7 @@ mod tests {
     fn test_reverse_operations() {
         let (mut signals, operations) = sample_input_2();
 
-        let op_results = reverse_operations(&operations);
+        let op_results = reverse_operations(&operations, &HashMap::new());
 
         assert_eq!(op_results.get(&String::from("z00")).unwrap(), &Operation::xor(String::from("bfw"), String::from("mjb"), String::from("z00")));
         assert_eq!(op_results.get(&String::from("kpj")).unwrap(), &Operation::or(String::from("pbm"), String::from("djm"), String::from("kpj")));
@@ -346,8 +554,8 @@ mod tests {
     fn test_deps() {
         let (mut signals, operations) = sample_input_2();
 
-        let op_results = reverse_operations(&operations);
-        assert_eq!(dependencies(String::from("z00"), &signals, &op_results), HashSet::from_iter([
+        let op_results = reverse_operations(&operations, &HashMap::new());
+        assert_eq!(dependencies(String::from("z00"), &op_results), HashSet::from_iter([
             String::from("bfw"), String::from("mjb"),
             String::from("ntg"), String::from("fgs"),
             String::from("y04"), String::from("y02"),
@@ -357,4 +565,53 @@ mod tests {
             String::from("x03"), String::from("x00"), 
         ]));
     }
+
+    #[test]
+    fn test_binary_format() {
+        assert_eq!(to_b(93, 7), vec![1, 0, 1, 1, 1, 0, 1]);
+        assert_eq!(to_b(39, 6), vec![1, 0, 0, 1, 1, 1]);
+        assert_eq!(to_b(140, 8), vec![1, 0, 0, 0, 1, 1, 0, 0]);
+
+        assert_eq!(from_b(to_b(93, 42)), 93);
+        assert_eq!(from_b(to_b(39, 42)), 39);
+        assert_eq!(from_b(to_b(140, 42)), 140);
+    }
+
+    /*
+    #[test]
+    fn test_execute_sum() {
+        let (_, operations) = sample_input_2();
+
+        let mut swaps = HashMap::<String, String>::new();
+        // swaps.insert(String::from("fgs"), String::from("psh"));
+        // swaps.insert(String::from("psh"), String::from("fgs"));
+        
+        let op_results = reverse_operations(&operations, &swaps);
+
+        for i in vec![(0, 2)] {
+            println!("=== {x} + {y} = {} ===", x + y);
+            
+            let (result, signals) = execute(&operations, x, y, &swaps);
+
+            let real = to_b(result, BITS + 1);
+            let expected = to_b(x + y, BITS + 1);
+            
+            for i in 0 .. BITS + 1 {
+                if real[i] != expected[i] {
+                    let j = BITS + 1 - i - 1;
+                    println!("DIFF z{j:02} expected: {} but is {}", expected[i], real[i]);
+                    let deps = dependencies_with_value(format!("z{j:02}"), &signals, &op_results);
+                    println!("");
+                }
+            }
+
+            println!(">>>>>> {real:?} {}", &signals.get(&String::from("z00")).unwrap_or(&0));
+            let deps = dependencies_with_value(format!("z00"), &signals, &op_results);
+
+        }
+        
+        
+        todo!()
+}
+    */
 }
